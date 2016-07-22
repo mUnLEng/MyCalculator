33ce2037ad46785b688f3ee2217adf778c23ddbf
= <=>
2016-07-22 21:46:38 +0800

    Linking files

---
+++ MyCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by Samson Pang on 22/7/16.
+//  Copyright © 2016 KProductions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
